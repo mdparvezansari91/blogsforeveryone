a95a32765db13d89918e68b9ac9746dd033dcb90
mdparvezansari91 <mdparvezansari91@gmail.com>
2024-11-15 11:44:44 +0400

    Update page.tsx

--- blogsforeveryone/src/app/signin/page.tsx
+++ blogsforeveryone/src/app/signin/page.tsx
@@ -60,13 +60,7 @@ function SigninContent({ dispatch, router }: SigninContentProps) {
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <Image
-                        alt="Your Company"
-                        src=""
-                        width={32}
-                        height={32}
-                        className="mx-auto h-10 w-auto"
-                    />
+
                     <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
